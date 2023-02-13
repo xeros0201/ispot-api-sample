94a51e1b49a905ab857668a80cab534910edb716
Toan Doan <toandev.95@gmail.com>
2023-02-13 14:26:46 +0700

    #1106

--- src/teams/teams.service.ts
+++ src/teams/teams.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
 
+import { PlayerEntity } from '../players/entities/player.entity';
+import { PlayersService } from '../players/players.service';
 import { SeasonEntity } from '../seasons/entities/season.entity';
+import { CreateTeamDto } from './dto/create-team.dto';
 import { TeamEntity } from './entities/team.entity';
 
 @Injectable()
 export class TeamsService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly playersService: PlayersService,
+  ) {}
+
+  public async findById(id: number): Promise<TeamEntity> {
+    return this.prismaService.team.findFirst({
+      where: { id },
+    });
+  }
+
+  public async create(data: CreateTeamDto): Promise<TeamEntity> {
+    return this.prismaService.team.create({ data });
+  }
 
   public async findAllBySeasonId(
     seasonId: SeasonEntity['id'],
@@ -14,5 +30,9 @@ export class TeamsService {
     return this.prismaService.team.findMany({
       where: { seasonId },
     });
+  }
+
+  public async findAllPlayers(id: number): Promise<PlayerEntity[]> {
+    return this.playersService.findAllByTeamId(id);
   }
 }
